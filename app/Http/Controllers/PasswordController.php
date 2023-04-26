d57511348c43281b03b66953a92af4d102e283f2
GaudinVincent <v.gaudin06@yahoo.fr>
2023-04-26 15:13:35 +0200

    wed 26/04 update passwordController

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -35,9 +35,9 @@ class PasswordController extends Controller
                 ->withErrors(['email' => __($status)]);
     }
     //on affiche le formulaire de réinitialisation du mdp
-    public function resetPassword(string $token)
+    public function resetPassword(Request $request, string $token)
     {
-        return view('password.resetForm')->with(['token' => $token]);
+        return view('password.resetForm')->with(['token' => $token, 'email' => $request->email]);
     }
     //on modifie le mdp dans la bdd
     public function updatePassword(Request $request)
@@ -53,36 +53,18 @@ class PasswordController extends Controller
                 'confirmed'
             ]
         ]);
-        $status = Password::reset(
-            $request->only(
-                'email',
-                'password',
-                'password_confirmation',
-                'token'
-            ),
-            //Pour chaque Model via $user
-            function ($user, $password) {
-                if ($user instanceof Professional) {
-                    $user->forceFill([
-                        'password' => Hash::make($password)
-                    ])->setRememberToken(Str::random(60));
-
-                    $user->save();
-                    event(new PasswordReset($user));
-
-                    //PATIENT
-                } elseif ($user instanceof Patient) {
-                    //on écrase la valeur précédente
-                    $user->forceFill([
-                        //on crypte à nouveau le mot de passe
-                        'password' => Hash::make($password)
-                        //on attribue un token temporaire à l'utilisateur
-                    ])->setRememberToken(Str::random(60));
-                    //puis on sauvegarde les nouvelles données dans la BDD
-                    $user->save();
-                    event(new PasswordReset($user));
-                    //ADMIN
-                } elseif ($user instanceof Admin) {
+        //on vérifie pour chaque modèle que l'adresse mail existe
+        if (Admin::where('email', $request->email)->exists()) {
+            //on définit quel "broker" utiliser dans le fichier auth
+            $status = Password::broker('admins')->reset(
+                $request->only(
+                    'email',
+                    'password',
+                    'password_confirmation',
+                    'token'
+                ),
+                //on exécute la fonction callback qui écrase l'ancien mdp
+                function ($user, $password) {
                     $user->forceFill([
                         'password' => Hash::make($password)
                     ])->setRememberToken(Str::random(60));
@@ -90,13 +72,47 @@ class PasswordController extends Controller
                     $user->save();
                     event(new PasswordReset($user));
                 }
-            }
-        );
+            );
+        } elseif (Professional::where('email', $request->email)->exists()) {
+            $status = Password::broker('professionals')->reset(
+                $request->only(
+                    'email',
+                    'password',
+                    'password_confirmation',
+                    'token'
+                ),
+                function ($user, $password) {
+                    $user->forceFill([
+                        'password' => Hash::make($password)
+                    ])->setRememberToken(Str::random(60));
+
+                    $user->save();
+                    event(new PasswordReset($user));
+                }
+            );
+
+        } else {
+            $status = Password::reset(
+                $request->only(
+                    'email',
+                    'password',
+                    'password_confirmation',
+                    'token'
+                ),
+                function ($user, $password) {
+                    $user->forceFill([
+                        'password' => Hash::make($password)
+                    ])->setRememberToken(Str::random(60));
+
+                    $user->save();
+                    event(new PasswordReset($user));
+                }
+            );
+        }
         return $status === Password::PASSWORD_RESET
             ? view('password.resetSuccess')
                 ->with('status', __($status))
                 ->with('redirectTo', 'http://localhost:3000/login')
             : back()->withErrors(['email' => [__($status)]]);
-
     }
 }
