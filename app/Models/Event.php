e96d3360d292027ff5e4b8990a87750260ff035d
PAROLDI-ALICE <aliceparoldi@gmail.com>
2023-04-28 09:20:06 +0200

    Fill in EventController&Model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -10,4 +10,16 @@ class Event extends Model
     use HasFactory;
     protected $guarded = ['id'];
 
+    protected $fillable = [
+        'date' => 'date_format:d/m/Y',
+        'time' => 'time',
+    ];
+    public function setCategoryAttribute($event)
+    {
+        $this->attributes['events'] = json_encode($event);
+    }
+    public function getCategoryAttribute($value)
+    {
+        return $this->attributes['events'] = json_decode($value);
+    }
 }
